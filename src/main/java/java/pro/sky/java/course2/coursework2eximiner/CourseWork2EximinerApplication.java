480e948c975f685b6214aa3738c5a07d16f40718
glinka <alexandraglinka@yandex.ru>
2023-07-30 20:29:02 +0300

    initial commit

---
+++ src/main/java/java/pro/sky/java/course2/coursework2eximiner/CourseWork2EximinerApplication.java
@@ -0,0 +1,13 @@
+package java.pro.sky.java.course2.coursework2eximiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2EximinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2EximinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/pro/sky/java/course2/coursework2eximiner/CourseWork2EximinerApplicationTests.java
@@ -0,0 +1,13 @@
+package java.pro.sky.java.course2.coursework2eximiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2EximinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
